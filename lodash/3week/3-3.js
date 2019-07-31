82da7772a7c4321f8423aa9a3657de15ee3526ef
phantom05 <monster2jy@gmail.com>
2019-07-31 12:14:49 +0900

    UPDATE lodash st

--- lodash/3week/3-3.js
+++ lodash/3week/3-3.js
@@ -7,3 +7,48 @@ var hello = function (name){
     return name+'hello'
   }
 }
+
+
+console.log(
+  _.chunk(Array.from(document.getElementsByTagName('li')),3)
+);
+console.log(
+  _.compact([0,1,false,'',3,null,undefined,'g'])
+);
+console.log(
+  _.concat([],2,[2],[[455],false])
+);
+console.log(
+  _.difference([2,1,9],[2,4,5])
+);
+console.log(
+  _.differenceBy([1.2,1.2,0.1],[2.3,3.4], _.filter(function(n){ return (n-1) > 1}) )
+);
+
+
+
+// 세번쨰 조건을 달대, 조건중 1개라도 undefined나 null이 나오게되면 성립이 안된다.
+
+
+
+var li1 = document.getElementById('hello').getElementsByTagName('li')
+var li2 = document.getElementById('world').getElementsByTagName('li')
+var li3 = [li1[0]];
+
+console.log(
+  _.differenceBy(li1,li2, function(n){
+    return n.getAttribute('class')
+  })
+);
+
+console.log(
+  _.difference(li1,li3)
+);
+console.log(
+  _.difference([{a:1},{b:2}],[{a:1},{c:3}])
+);
+// 참조든 원시든 잘 비교한다.
+
+console.log(
+  _.concat(Array.from(li1),Array.from(li2))
+);
