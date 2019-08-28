a549a555f0c2333956520393af820f5087af3361
phantom05 <monster2jy@gmail.com>
2019-08-28 10:15:32 +0900

    업데이트 로대쉬

--- lodash/1week/1-2.js
+++ lodash/1week/1-2.js
@@ -24,7 +24,7 @@ console.log(
   _.every(users,'user')
 );
 
-console.clear()
+console.clear() 
 var users = [
   { 'user': 'barney', 'age': 36, 'active': true },
   { 'user': 'fred',   'age': 40, 'active': false }
